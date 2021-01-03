a8adf98fc7164b6cf39275e22dcb603a8b1e1ba5
Parag <pal.parag257@gmail.com>
2021-01-03 16:37:52 +0530

    1st

--- 2.dart
+++ 2.dart
@@ -7,10 +7,13 @@ void main() {
   list2[2] = "3rd Element";
   print(list2);
 
-  var numbers = [1,2,3,4,5,6,7,8,9,0];
-  for (var n in numbers){
-    print(n);  
+  var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  for (var n in numbers) {
+    print(n);
   }
 
   // Javascript is everywhere
+  numbers.forEach((n) => {
+    print(n)
+  });
 }
